9bdf4d8818e50762b803cc3b994a0152e17a6eb4
DESKTOP-8UD5B0S\MS SocialNUI <haust798@gmail.com>
2018-07-05 15:40:48 +1000

    First commit

---
+++ SecondaryTask/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SecondaryTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLetter = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // txtLetter
+            // 
+            this.txtLetter.BackColor = System.Drawing.SystemColors.Control;
+            this.txtLetter.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLetter.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtLetter.Location = new System.Drawing.Point(25, 36);
+            this.txtLetter.Name = "txtLetter";
+            this.txtLetter.ReadOnly = true;
+            this.txtLetter.Size = new System.Drawing.Size(198, 158);
+            this.txtLetter.TabIndex = 1;
+            this.txtLetter.TabStop = false;
+            this.txtLetter.Text = "A";
+            this.txtLetter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLetter.Click += new System.EventHandler(this.txtLetter_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 229);
+            this.Controls.Add(this.txtLetter);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtLetter;
+        public System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ SecondaryTask/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace SecondaryTask
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer t;
+        Stopwatch watch = Stopwatch.StartNew();
+        int[] intervals = {5000,10000,50000,100000};
+        int index = 0;
+        StreamWriter writer = new StreamWriter("C:\\Users\\MS SocialNUI\\Desktop\\Downloads\\KBTest.txt");
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            writer.WriteLine(GetTimestamp(DateTime.UtcNow) + "   " + "Test started");
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            txtLetter.ForeColor = Color.Red;
+            writer.WriteLine(GetTimestamp(DateTime.UtcNow) + "   " + "Text color changed");
+        }
+
+        private void txtLetter_Click(object sender, EventArgs e)
+        {
+            writer.WriteLine(GetTimestamp(DateTime.UtcNow) + "   " + "Clicked");
+
+            // To make the focus disappear
+            txtLetter.Enabled = false;
+            txtLetter.Enabled = true;
+
+            t = new System.Timers.Timer();
+            if (index < intervals.Length)
+            {
+                t.Interval = intervals[index];
+                Console.WriteLine("Interval is: "+ intervals[index]);
+                index += 1;
+            }
+            t.Start();
+            t.Elapsed += OnTimeEvent;
+            txtLetter.ForeColor = Color.Black;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t != null) { t.Stop(); }
+            writer.WriteLine(GetTimestamp(DateTime.UtcNow) + "   " + "Test completed");
+            Application.DoEvents();
+            writer.Close();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine("Diff: " + serialPort1.ReadLine());
+            try
+            {
+                writer.WriteLine(GetTimestamp(DateTime.UtcNow) + "   " + serialPort1.ReadLine());
+            }
+            catch (System.Exception ex){}
+        }
+
+        private long GetTimestamp(DateTime dateTime)
+        {
+            DateTime dt1970 = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return (dateTime.Ticks - dt1970.Ticks) / 10000;
+        }
+    }
+}
